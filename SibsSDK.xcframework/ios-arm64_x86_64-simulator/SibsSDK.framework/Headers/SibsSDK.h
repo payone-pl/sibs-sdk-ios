2756b26f60effa711be5dc57af653a2cc0750690
Marcin Kuświk <marcin@littlebigdesign.pl>
2022-12-30 04:29:40 +0100

    Library file version 1.0.0

---
+++ SibsSDK.xcframework/ios-arm64_x86_64-simulator/SibsSDK.framework/Headers/SibsSDK.h
@@ -0,0 +1,18 @@
+//
+//  SibsSDK.h
+//  SibsSDK
+//
+//  Created by Marcin Kuświk on 29/12/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SibsSDK.
+FOUNDATION_EXPORT double SibsSDKVersionNumber;
+
+//! Project version string for SibsSDK.
+FOUNDATION_EXPORT const unsigned char SibsSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SibsSDK/PublicHeader.h>
+
+
